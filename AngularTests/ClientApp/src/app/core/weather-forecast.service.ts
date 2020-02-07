03285d339a5d939b8cf6a0fbc1d87566e2ebe255
Marcin Misiek <misiek_marcin@yahoo.com>
2020-02-07 13:54:19 +0100

    Added: simple cache

--- AngularTests/ClientApp/src/app/core/weather-forecast.service.ts
+++ AngularTests/ClientApp/src/app/core/weather-forecast.service.ts
@@ -1,6 +1,8 @@
 import { Injectable, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { WeatherForecast } from '../types/weather-forecast';
+import { Observable, EMPTY } from 'rxjs';
+import { map, publishReplay, refCount, catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -8,12 +10,26 @@ import { WeatherForecast } from '../types/weather-forecast';
 export class WeatherForecastService {
   public baseUrl = '/api';
 
-  constructor(private httpClient: HttpClient, @Inject('BASE_URL') baseUrl: string) { 
+  private weatherForecasts: Observable<WeatherForecast[]>;
+
+  constructor(private httpClient: HttpClient, @Inject('BASE_URL') baseUrl: string) {
     this.baseUrl = baseUrl;
   }
 
-  public getAll() {
-    return this.httpClient.get<WeatherForecast[]>(this.baseUrl + 'weatherforecast');
-  }
+  public getAll(): Observable<WeatherForecast[]> {
+    if (this.weatherForecasts) { return this.weatherForecasts; }
 
+    this.weatherForecasts = this.httpClient
+      .get<WeatherForecast[]>(this.baseUrl + 'weatherforecast')
+      .pipe(
+        publishReplay(1),
+        refCount(),
+        catchError(error => {
+          delete this.weatherForecasts;
+          return EMPTY;
+        })
+      );
+
+    return this.weatherForecasts;
+  }
 }
