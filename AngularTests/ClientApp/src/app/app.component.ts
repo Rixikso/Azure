b034644e3dd61af09361bc0cd7a545ba86dac046
Marcin Misiek <misiek_marcin@yahoo.com>
2020-02-10 16:45:06 +0100

    Added: environments

--- AngularTests/ClientApp/src/app/app.component.ts
+++ AngularTests/ClientApp/src/app/app.component.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  public ngOnInit(): void {
+    const urlName = environment.apiUrl;
+    console.log(urlName);
+  }
 }
--- AngularTests/ClientApp/src/app/components/user-list/user-list.component.ts
+++ AngularTests/ClientApp/src/app/components/user-list/user-list.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserListComponent implements OnInit {
   public inputValue: string = '';
+  
   constructor() { }
 
   ngOnInit() {
