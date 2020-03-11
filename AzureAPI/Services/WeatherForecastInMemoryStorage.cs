3c9a98e5ecdfbb174ff1cdc47433997251e32ebe
Marcin Misiek <misiek_marcin@yahoo.com>
2020-03-11 13:27:25 +0100

    Added: simple CRUD API

---
+++ AzureAPI/Services/WeatherForecastInMemoryStorage.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AzureAPI.Services
+{
+    public class WeatherForecastInMemoryStorage : IInMemoryStorage<WeatherForecast>
+    {
+        public Task AddAsync(WeatherForecast item)
+        {
+            if (!_weatherForecasts.Contains(item))
+            {
+                _weatherForecasts.Add(item);
+            }
+
+            return Task.CompletedTask;
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            var forecastToDelete = _weatherForecasts.FirstOrDefault(forecast => forecast.Id == id);
+            _weatherForecasts.Remove(forecastToDelete);
+
+            return Task.CompletedTask;
+        }
+
+        public Task<WeatherForecast> GetAsync(int id)
+        {
+            var forecast = _weatherForecasts.FirstOrDefault(forecast => forecast.Id == id);
+
+            return Task.FromResult(forecast);
+        }
+
+        public Task<IEnumerable<WeatherForecast>> GetAllAsync()
+        {
+            return Task.FromResult(_weatherForecasts.AsEnumerable());
+        }
+
+        private List<WeatherForecast> _weatherForecasts = new List<WeatherForecast>();
+    }
+}
--- AzureAPI/WeatherForecast.cs
+++ AzureAPI/WeatherForecast.cs
@@ -4,6 +4,8 @@ namespace AzureAPI
 {
     public class WeatherForecast
     {
+        public int Id { get; set; }
+
         public DateTime Date { get; set; }
 
         public int TemperatureC { get; set; }
